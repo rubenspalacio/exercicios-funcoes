a49a6a2ab22a56df33ee7b9f7913874e53ba82f9
Rubens Palacio <scaurirubens@gmail.com>
2022-12-18 19:24:07 -0300

    Primeiro commit dos exercícios de funções.

---
+++ testeFuncao.js
@@ -0,0 +1,58 @@
+// function acharDescontoEmPorcentagem(n1, n2) {
+// let porcentagem = (n1 / n2) * 100
+// console.log("A porcentagem é de " + porcentagem + "%")
+// }
+
+// acharDescontoEmPorcentagem(20, 80);
+
+//==================== Função achar ROI ===================
+
+// function acharRoi(faturamento, investimento){
+//     return (faturamento / investimento) * 100
+// }
+
+// var roi = acharRoi(24000, 2000);
+// console.log("O ROI foi de " + roi + "%.")
+
+
+
+//===================== Cartão de Visita =====================
+
+
+// function cartaoDeVisita(nome, sobrenome){
+//     console.log("Cartão de visita\n" + nome + " " + sobrenome)
+// }
+// cartaoDeVisita("Rubens", "Scauri")
+
+
+//================== Autonomia do Veículo ====================
+
+// function autonomiaDoVeiculo(distanciaEmKm, litros){
+//     return distanciaEmKm / litros
+// }
+
+// var autonomia = autonomiaDoVeiculo(100, 8);
+// console.log("A autonomia do veículo é de " + autonomia + " Km por litro.")
+
+//================= Exercícios ======================
+// 1 - Retornar sempre a metado do número recebido como parâmetro:
+
+// function metade(numero){
+//     return numero / 2
+// }
+
+// var resultado = metade(8);
+// console.log(resultado)
+
+
+// 2 - Lavanderia
+
+function calculoValor(quantidadeKgRoupa){
+    return quantidadeKgRoupa * 5
+}
+
+var valorDevido = calculoValor(12);
+console.log("Valor para lavar sua roupa é de R$ " + valorDevido + ".");
+
+
+
